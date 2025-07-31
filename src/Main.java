28c3005fc2dbcba1da26de0cd1d71ef00a1354f5
Ben <benh6800@outlook.com>
2025-07-31 09:40:24 -0700

    made intro and added files for each game and for the ATM

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+    public static void main (String [] args) {
+        Scanner Input = new Scanner(System.in);
+
+        //variables
+
+        String name;
+
+        System.out.println("============================");
+        System.out.println("    welcome to the casino   ");
+        System.out.println("============================");
+        System.out.print("\nwhat is your name: ");
+        name = Input.nextLine();
+        System.out.println("hello " + name + ".");
+
+
+        //roulette
+    }
+    public static void printGames () {
+        System.out.print("");
+
+    }
+}
