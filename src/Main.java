bcf00a60ae4a2850e09db870d3dd803825a01c8d
Ben <benh6800@outlook.com>
2025-07-31 10:36:51 -0700

    finished the intro and menu for the casino
    note: when the games are ready uncomment the stuff in the switch also calling roulette is turning in a error. IDK whats happening.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,18 @@
 import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.Arrays;
 
 public class Main {
     public static void main (String [] args) {
         Scanner Input = new Scanner(System.in);
 
-        //variables
-
+        //Defining stuff
+        String[] thingsToDo = {"Roulette", "Black Jack", "Slot Machine", "ATM", "Exit casino"};
+        int money = 100;
+        int bankAccount = 1400;
         String name;
+        String activity;
+        boolean playing = true;
 
+        //welcome code
         System.out.println("============================");
         System.out.println("    welcome to the casino   ");
         System.out.println("============================");
@@ -17,11 +20,30 @@ public class Main {
         name = Input.nextLine();
         System.out.println("hello " + name + ".");
 
+        while (playing) {
+            printGames(thingsToDo);
+            System.out.print("would you like to do(inter the number): ");
+            activity = Input.nextLine();
 
-        //roulette
+            switch (activity) {
+                case "1": //Roulette
+                    //roulette.main();
+                case "2": //Black Jack
+                    //blackJack.main();
+                case "3": //Slots
+                    //slots.main();
+                case "4": //ATM
+                    //ATM.main();
+                case "5": //Exit
+                    playing = false;
+            }
+        }
     }
-    public static void printGames () {
-        System.out.print("");
+    public static void printGames(String[] thingsToDo) {
+        System.out.print("\nthings to do");
 
+        for (int i = 0; i<thingsToDo.length; i++) {
+            System.out.println((i + 1) + ". " + thingsToDo[i]);
+        }
     }
 }
