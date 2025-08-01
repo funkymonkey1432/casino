95b4aad3838b9ccd1012008687bc203aca33d2b2
Ben <brnason.benjamin9@gmail.com>
2025-07-31 17:24:46 -0700

    Added baccarat continued roulette

--- src/roulette.java
+++ src/roulette.java
@@ -1,4 +1,4 @@
-//TODO: ben put all the win and lose statements together and make everything a switch case
+
 import java.util.*;
 
 public class roulette {
@@ -41,72 +41,85 @@ public class roulette {
             System.out.print("parity (odd/even), thirds, a number or colours (Type one of the 4 given options): ");
             String choice = scanner.next();
             choice = choice.toLowerCase();
-            if (choice.equals("parity")) {
-                System.out.print("odd or even: ");
-                String bet = scanner.next().toLowerCase();
-                System.out.println("The result is: " + result);
 
-                if ((bet.equals("even") && result % 2 == 0 && 0 != result) ||
-                    (bet.equals("odd") && result % 2 == 1 && 0 != result)) {
-                    System.out.println("You win!\nBet: " + wager + "\nPayout: Your bet + " + wager + "$");
-                    budget += 2 * wager;
-                    System.out.println("Current budget: " + budget + "$");
-                } else if ((bet.equals("even") && result % 2 == 1) ||
-                          (bet.equals("odd") && result % 2 == 0)){
-                    System.out.println("You lose!\nCurrent budget: " + budget + "$");
-                } else {
-                    System.out.println("invalid input");
-                }
+            switch (choice){
+                    case "parity":
+                        System.out.print("odd or even: ");
+                        String parityBet = scanner.next().toLowerCase();
+                        System.out.println("The result is: " + result);
 
-            } else if (choice.equals("number")) {
-                System.out.print("Chose your number (0-32): ");
-                int bet = scanner.nextInt();
-                if (bet == result) {
-                    budget += 33 * wager;
-                    System.out.println("You win!\nBet: " + wager + "$\nPayout: " + (33 * wager) + "$");
-                } else {
-                    System.out.println("You lose!\nCurrent budget: " + budget + "$");
-                }
+                        if (((parityBet.equals("even") && result % 2 == 0 && 0 != result) ||
+                            (parityBet.equals("odd") && result % 2 == 1 && 0 != result)) && result!=0) {
+                            System.out.println("You win!\nBet: " + wager + "\nPayout: Your bet + " + wager + "$");
+                            budget += 2 * wager;
+                            System.out.println("Current budget: " + budget + "$");
+                        } else if (((parityBet.equals("even") && result % 2 == 1) ||
+                                  (parityBet.equals("odd") && result % 2 == 0)) && result!=0){
+                            System.out.println("You lose!\nCurrent budget: " + budget + "$");
+                        } else if (result == 0) {
+                            System.out.println("The house wins. Tough luck!");
+                        }else {
+                            System.out.println("invalid input");
+                        }
+                        break;
+                    case "number":
+                        System.out.print("Chose your number (0-32): ");
+                        int numberBet = scanner.nextInt();
+                        if (numberBet == result) {
+                            budget += 33 * wager;
+                            System.out.println("You win!\nBet: " + wager + "$\nPayout: " + (33 * wager) + "$");
+                        } else if (result == 0){
+                            System.out.println("The house wins. Tough luck!");
+                        } else {
+                            System.out.println("You lose!\nCurrent budget: " + budget + "$");
+                        }
+                        break;
+                    case "thirds":
+                        System.out.print("bottom,mid or top: ");
+                        String thirdsBet = scanner.next().toLowerCase();
+                        switch (thirdsBet){
+                            case "bottom":
+                                if (result <= 10) {
+                                    System.out.println("You win!\nBet: " + wager + "$\nPayout: " + (2 * wager) + "$");
+                                    budget += 2* wager;
+                                } else {
+                                    System.out.println("You lose!\nCurrent budget: " + budget + "$");
+                                }break;
 
-            } else if (choice.equals("thirds")) {
-                System.out.print("bottom,mid or top: ");
-                String thirdsChoice = scanner.next().toLowerCase();
-                if (thirdsChoice.equals("bottom")) {
-                    if (result <= 10) {
-                        System.out.println("You win!\nBet: " + wager + "$\nPayout: " + (2 * wager) + "$");
-                        budget += 2* wager;
-                    } else {
-                        System.out.println("You lose!\nCurrent budget: " + budget + "$");
-                    }
-                }
-                if (thirdsChoice.equals("mid")) {
-                    if (result > 10 && result < 22) {
-                        System.out.println("You win!\nBet: " + wager + "$\nPayout: " + (2 * wager) + "$");
-                        budget+=2*wager;
-                    } else {
-                        System.out.println("You lose!\nCurrent budget: " + budget + "$");
-                    }
-                }
-                if (thirdsChoice.equals("top")) {
-                    if (result > 21) {
-                        System.out.println("You win!\nBet: " + wager + "$\nPayout: " + (2 * wager) + "$");
-                        budget+=2*wager;
-                    } else {
-                        System.out.println("You lose!\nCurrent budget: " + budget + "$");
-                    }
-                }
-            }else if (choice.equals("colours")){
-                System.out.print("Red or Black: ");
-                String bet = scanner.next();
-                bet = bet.toLowerCase();
-                System.out.println(bet);
-                System.out.println("Its a "+rouletteTable.get(result)+" "+result+".");
-                if ((rouletteTable.get(result).equals("red") && bet.equals("red")) || (rouletteTable.get(result).equals("black") && bet.equals("black"))){
-                    System.out.println("You win!\nBet: " + wager + "\nPayout: Your bet + " + wager + "$");
-                    budget+=2*wager;
-                }else{
-                    System.out.println("You lose!\nCurrent budget: " + budget + "$");
-                }
+                            case "mid":
+                                if (result > 10 && result < 22) {
+                                    System.out.println("You win!\nBet: " + wager + "$\nPayout: " + (2 * wager) + "$");
+                                    budget+=2*wager;
+                                } else {
+                                    System.out.println("You lose!\nCurrent budget: " + budget + "$");
+                                }break;
+
+                            case "top":
+                                if (result > 21) {
+                                    System.out.println("You win!\nBet: " + wager + "$\nPayout: " + (2 * wager) + "$");
+                                    budget+=2*wager;
+                                }else if (result == 0) {
+                                    System.out.println("The house wins. Tough luck!");
+                                }else {
+                                    System.out.println("You lose!\nCurrent budget: " + budget + "$");
+                                }break;
+                        }
+                        break;
+                    case "colours":
+                        System.out.print("Red or Black: ");
+                        String bet = scanner.next();
+                        bet = bet.toLowerCase();
+                        System.out.println(bet);
+                        System.out.println("Its a "+rouletteTable.get(result)+" "+result+".");
+                        if ((rouletteTable.get(result).equals("red") && bet.equals("red")) || (rouletteTable.get(result).equals("black") && bet.equals("black"))){
+                            System.out.println("You win!\nBet: " + wager + "\nPayout: Your bet + " + wager + "$");
+                            budget+=2*wager;
+                        }else if (result == 0) {
+                            System.out.println("The house wins. Tough luck!");
+                        }else{
+                            System.out.println("You lose!\nCurrent budget: " + budget + "$");
+                        }
+                        break;
             }
             System.out.print("Do you want to continue? yes or no:  ");
             String keepGoing = scanner.next();
