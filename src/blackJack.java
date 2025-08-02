8780ff3ca1c46b99c273ba28e62cad5901b5b3ab
ben H <benh6800@outlook.com>
2025-08-02 09:49:50 -0700

    did a little work on the inter face and variables

--- src/blackJack.java
+++ src/blackJack.java
@@ -7,8 +7,12 @@ public class blackJack {
     static ArrayList<Integer> playerHand = new ArrayList<>();
     static ArrayList<Integer> dealerHand = new ArrayList<>();
 
+
     static int playerSum = 0;
     static int playerAces = 0;
+
+    static int dealerSum = 0;
+    static int dealerAces = 0;
 
     static int newPlayerCard;
     static int newDealerCard;
@@ -24,17 +28,31 @@ public class blackJack {
 
 
         ArrayList<Integer> deck = deckBuilder();
+
+        //draws the players starting cards
         drawCardPlayer(deck);
+        drawCardPlayer(deck);
+
+        //draws the dealers starting cards
+        drawCardDealer(deck);
+        drawCardDealer(deck);
+
+        System.out.println("dealer shows: " + (dealerSum - newDealerCard);
+
+        System.out.println("Aces: " + playerAces);
+        System.out.println("hand: " + playerHand);
+
+
+
+
 
 
 
     }
 
-
-
     public static ArrayList<Integer> deckBuilder () {
         ArrayList<Integer> deck = new ArrayList<>();
-        for (int i = 0; i < 52; i++) {
+        for (int i = 0; i < deck.size(); i++) {
             int value = (i % 13) + 1;
             if (value <= 10 && value != 1) {
                 deck.add(value);
@@ -52,9 +70,14 @@ public class blackJack {
         int randomNumber = random.nextInt(deck.size());
         newPlayerCard = deck.get(randomNumber);
         deck.remove(randomNumber);
+
         System.out.println("you drew a " + newPlayerCard);
         playerHand.add(newPlayerCard);
         return newPlayerCard;
+
+        for (int i = 0; 1 < playerHand.size(); i++) {
+            playerSum += dealerHand.get(i);
+        }
     }
 
     public static Integer drawCardDealer(ArrayList<Integer> deck) {
